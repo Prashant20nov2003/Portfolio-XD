a0496db363b25c039a9c63f8b35abbc024238854
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-04-20 04:11:54 +0530

    removed depricated listners

--- src/hooks/useMediaQuery.tsx
+++ src/hooks/useMediaQuery.tsx
@@ -1,41 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 
 const getMatches = (query: string): boolean => {
   // Prevents SSR issues
   if (typeof window !== 'undefined') {
-    return window.matchMedia(query).matches
+    return window.matchMedia(query).matches;
   }
-  return false
-}
+  return false;
+};
 
 export function useMediaQuery(query: string): boolean {
-  const [matches, setMatches] = useState<boolean>(getMatches(query))
+  const [matches, setMatches] = useState<boolean>(getMatches(query));
 
   useEffect(() => {
-    function handleChange() {
-      setMatches(getMatches(query))
-    }
+    const matchMedia: MediaQueryList = window.matchMedia(query);
 
-    const matchMedia = window.matchMedia(query)
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches);
+    };
 
-    // Triggered at the first client-side load and if query changes
-    handleChange()
+    // Set initial value
+    setMatches(matchMedia.matches);
 
-    // Use deprecated `addListener` and `removeListener` to support Safari < 14 (#135)
-    if (matchMedia.addListener) {
-      matchMedia.addListener(handleChange)
-    } else {
-      matchMedia.addEventListener('change', handleChange)
-    }
+    matchMedia.addEventListener('change', handleChange);
 
     return () => {
-      if (matchMedia.removeListener) {
-        matchMedia.removeListener(handleChange)
-      } else {
-        matchMedia.removeEventListener('change', handleChange)
-      }
-    }
-  }, [query])
+      matchMedia.removeEventListener('change', handleChange);
+    };
+  }, [query]);
 
-  return matches
+  return matches;
 }
